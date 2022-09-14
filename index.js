5e460cafe907f9e219310603d357bec66d9ea223
Ian <ian.wiedenman@gmail.com>
2022-09-13 20:52:04 -0700

    Update index.js

--- index.js
+++ index.js
@@ -5,8 +5,21 @@ if (!argv.project) {
   return console.error("Please provide a project argument like: npm start -- --project='my-project'");
 }
 
-const parentCommand = `gcloud container images list --repository=us.gcr.io/${argv.project}/gcf/us-central1`;
-const childCommand = `gcloud container images delete us.gcr.io/${argv.project}/gcf/us-central1/{id}/{finalFolder}:latest --force-delete-tags  --quiet`
+// DELETE
+https://console.cloud.google.com/gcr/images/giftly-application/us/gcf-artifacts?project=giftly-application
+https://console.cloud.google.com/artifacts?project=giftly-application
+
+// https://us.gcr.io/giftly-application/gcf-artifacts/bm__api
+// gcloud container images list --repository=us.gcr.io/giftly-application/gcf-artifacts
+// gcloud container images list --project="giftly-application"
+
+// const parentCommand = `gcloud container images list --repository=us.gcr.io/${argv.project}/gcf/us-central1`;
+const parentCommand = `gcloud container images list --repository=us.gcr.io/${argv.project}/gcf-artifacts`;
+// const childCommand = `gcloud container images delete us.gcr.io/${argv.project}/gcf/us-central1/{id}/{finalFolder}:latest --force-delete-tags --quiet`
+const childCommand = `gcloud container images delete us.gcr.io/${argv.project}/gcf/us-central1/{id}/{finalFolder}:latest --force-delete-tags --quiet`
+
+// gcloud container images delete us.gcr.io/giftly-application/gcf-artifacts/bm__api:latest --force-delete-tags --quiet
+// gcloud container images delete us.gcr.io/giftly-application/gcf-artifacts/bm__api:latest
 
 exec(parentCommand, function (error, stdout, stderr) {
   let folders = stdout.split('\n');
